842871e6fc10ca9fd3984bd92a7350ed89b010ec
Kylian2 <kylian.richard@gmail.com>
2024-04-15 21:46:57 +0200

    page meslivres + composant Book

---
+++ books-front/components/Book.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="border-2 border-gray-950 w-72 flex flex-col p-2">
+        <img :src="`${$attrs.book.cover}`" alt="book-cover">
+        <div class="my-2">
+            <h3 class="text-xl font-medium">{{$attrs.book.title}}</h3>
+            <h4 class="text-lg font-normal">{{$attrs.book.author}}</h4>
+        </div>
+        <NuxtLink class="w-full bg-green-500 cursor-pointer px-4 py-2 transition-all duration-700 hover:bg-green-600" to="">Voir en details</NuxtLink>
+    </div>
+</template>
+<script setup lang="ts">
+const book = useAttrs().book
+</script>
--- books-front/pages/meslivres.vue
+++ books-front/pages/meslivres.vue
@@ -1,8 +1,33 @@
 <template>
     <HeaderLog/>
+    <h1 class="text-3xl font-bold my-12 mx-20">Ma Collection</h1>
+    <div class="flex gap-8 mx-auto flex-wrap w-10/12 justify-center">
+        <Book v-if="books !== null" v-for="book in books" :key="book.id" :book="book"/>
+    </div>
 </template>
 <script setup lang="ts">
+
+import { getToken } from '~/composable/token';
+
 definePageMeta({
     middleware: ["auth", "redirect"]
 })
+
+let books;
+
+if(process.client){
+
+    const token = getToken()
+
+    await $fetch(`http://localhost:3333/books`, {
+        method: 'get',
+        headers : {
+            Authorization: `Bearer ${token}`,
+        }
+    }).then((response) => {
+        books = response
+        console.log(books)
+    })
+
+}
 </script>
