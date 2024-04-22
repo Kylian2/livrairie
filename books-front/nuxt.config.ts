b1af0b8cae87fc93ea893e0a4d9e1b0cd6af97f6
Kylian2 <kylian.richard@gmail.com>
2024-04-22 22:25:19 +0200

    modif production baseUrl

--- books-front/nuxt.config.ts
+++ books-front/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
-      baseUrl: process.env.NODE_ENV === "production" ? "http://165.232.117.216" : "http://localhost:3333"
+      baseUrl: process.env.NODE_ENV === "production" ? "http://165.232.117.216:3333" : "http://localhost:3333"
     }
   }
 })
